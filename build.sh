35cdfe96c2ee1ed9d2804036d3b0304a9decf314
Karsten Gebbert <k@ioctl.it>
2016-07-26 16:46:53 -0700

    adding zmq

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if test "$OS" = "Windows_NT"
 then
   # use .Net
@@ -6,28 +6,16 @@ then
   .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
+    exit $exit_code
   fi
 
   .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
+    exit $exit_code
   fi
 
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else
-  # use mono
-  mono .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  mono .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
   mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
